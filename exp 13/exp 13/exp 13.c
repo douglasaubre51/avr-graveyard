25662decb27db945d0f421ff71b6325aafa56914
Allen Bose <boseallen192@gmail.com>
2025-10-07 09:46:06 +0530

    added exp 13

--- exp 13/exp 13/exp 13.c
+++ exp 13/exp 13/exp 13.c
@@ -9,5 +9,20 @@
 
 int main(void)
 {
+  DDRD = 0xff;
+  DDRB = 0xff;
+  DDRA = 0;
   
+  ADCSRA = 0x87;
+  ADMUX = 0xe0;
+  
+  while(1){
+	  ADCSRA |= (1 << ADSC);
+	  
+	  while((ADCSRA & (1 << ADIF)) == 0);
+	  
+	  PORTB = ADCH;
+  }
+  
+  return 0;
 }
